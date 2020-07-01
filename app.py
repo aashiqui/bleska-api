76b501309ecefee46bbc9daf3e9bd9b988d80a30
aashiqui <lolenzerf@gmail.com>
2020-07-02 01:43:46 +0530

    First Commit

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello():
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+    return "Hello  test {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
